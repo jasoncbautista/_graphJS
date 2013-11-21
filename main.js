c1b43755f42aba540dee253590ad336dcc2f10f1
Jason <jason.bautista@sqor.com>
2013-11-21 09:27:41 -0800

    Rendering basics

--- main.js
+++ main.js
@@ -5,16 +5,27 @@ var mockData = {
     ,   {name: "Michael", value: 189}
     ,   {name: "Magic", value: 100}
     ]
-
 }
+
+var drawPlayer = function(player) {
+    // Getting our template:
+    var templateString = $("#playerBubble").html();
+
+    console.log(player);
+    console.log(templateString);
+    var playerCardEl = _.template(templateString)(player)
+
+    return $(playerCardEl);
+
+};
+
 $(document).ready(function() {
     var players = mockData.players;
 
-
     _.each(players, function(player) {
-        console.log(player);
+        var playerHTML = drawPlayer(player);
+        $('body').append(playerHTML);
     });
-
 
 });
 
